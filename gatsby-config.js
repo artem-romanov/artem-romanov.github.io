94fa0468783924bcf02c88c25fe4fd26d891fc99
Romanov Artem <xenox2048@gmail.com>
2018-07-22 00:47:01 +0300

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/*eslint-disable no-undef */
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    // 'gatsby-plugin-eslint'
+  ],
+}
