13f3a8fcfa6c40c86365462b326c20c29d1d5602
Romanov Artem <xenox2048@gmail.com>
2018-07-22 21:38:21 +0300

    Add deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,10 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
+  pathPrefix: "/homepage",
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
   ],
-}
+};
+
